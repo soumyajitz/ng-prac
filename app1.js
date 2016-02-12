0078ff74a6e21daf5d15cc70934ed5eff20144fc
soumyajitz <2014soumyajit@gmail.com>
2016-02-11 23:47:27 -0600

    Created own service called nameService

--- app1.js
+++ app1.js
@@ -9,17 +9,43 @@ myApp.config(function($routeProvider){
         controller : 'mainController'
     })
     
+    .when('/second',{
+        templateUrl : 'pages/second.html',
+        controller : 'secondController'
+    })
+    
+    
     .when('/second/:num',{
         templateUrl : 'pages/second.html',
         controller : 'secondController'
     })
-})
+});
 
-myApp.controller('mainController',['$scope','$log',function($scope,$log){
-   $scope.name = "Hello";
+myApp.service('nameService',function(){
+
+    var self = this;
+    this.name = "Soumyajit";
+    
+    this.nameLength = function(){
+        return self.name.length;
+    }
+    
+});
+
+
+myApp.controller('mainController',['$scope','$log','nameService',function($scope,$log,nameService){
+   $scope.name = nameService.name;
+    
+    $scope.$watch('name',function(){
+        nameService.name = $scope.name;
+    })
+    
+    $log.log(nameService.name);
+    $log.log(nameService.nameLength());
+    
 }]);
 
-myApp.controller('secondController',['$scope','$log','$routeParams',function($scope,$log,$routeParams){
-   $scope.name = "World----";
-   $scope.num = $routeParams.num;
+myApp.controller('secondController',['$scope','$log','$routeParams','nameService',function($scope,$log,$routeParams,nameService){
+   $scope.name = nameService.name;
+   $scope.num = $routeParams.num || 1;
 }]);
